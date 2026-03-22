1b4100036fe20ba1c4bd29ddf7346870f9d5b86a
Dorijan Rijavec <rijavecdorijan@gmail.com>
2026-03-22 11:29:03 +0100

    removed due to unprocessed images being uploaded to cloud

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,16 @@ app.use("/uploads", express.static(UPLOADS_DIR));
 import authRoutes from "./routes/auth.js";
 import photoRoutes from "./routes/photos.js";
 import promptRoutes from "./routes/prompt.js";
+import userRoutes from "./routes/users.js";
+
+app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/photos", photoRoutes);
 app.use("/api/prompts", promptRoutes);
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
 
-app.get("/api/db-health", async (req, res) => {
+app.get("/api/db-health", async (_req, res) => {
     try {
       const { rows } = await pool.query("SELECT 1 AS ok");
       res.json({ ok: true, rows });
